d07c28e06736d6c8550ae1da126cbfb4d1be9207
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-10 15:05:06 -0400

    Typescript app fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import './App.css';
 import React from 'react';
-
-import { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  let sudoku = [[0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-  const [grid, setGrid] = useState(sudoku)
-
-
-  const drawGrid = () => {
-    console.log(grid);
-  }
-
-
   return (
     <div className="App">
-      <p className={"text-3xl font-bold underline"}>
-        {/* {drawGrid()} */}
-      </p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
