831a573f071903bf81509e2c87ea33a906584c94
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-11 13:37:54 -0400

    Added bordering to numbers. Thinking sudoku grid should be converted from nested divs/spans into a table.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 function App() {
 
-  let sudoku = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]];
+  const sudoku = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]];
 
   const [grid, setGrid] = useState(sudoku);
 
@@ -13,60 +13,62 @@ function App() {
   const drawGrid = () => {
     let final_html: any = [];
     let can_break: boolean = false;
+    //Begin looping over the sudoku array
     for (let i = 1; i < grid.length + 1; i++) {
+      //We need to break every 3 rows to form our sudoku square..
       if (i % 3 === 0) {
-        console.log('hi')
         can_break = true;
       } else {
         can_break = false;
       }
-
+      //Build the JSX/HTML
       final_html.push(
         <div key={"grid_" + i} className="grid-1">
           <div className="first-row">
-            <span>
-              {grid[i - 1][0]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][0]}</span>
             </span>
-            <span>
-              {grid[i - 1][1]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][1]}</span>
             </span>
-            <span>
-              {grid[i - 1][2]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][2]} </span>
             </span>
           </div>
           <div className="second-row">
-            <span>
-              {grid[i - 1][3]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][3]}</span>
             </span>
-            <span>
-              {grid[i - 1][4]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][4]}</span>
             </span>
-            <span>
-              {grid[i - 1][5]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][5]}</span>
             </span>
           </div>
           <div className="third-row">
-            <span>
-              {grid[i - 1][6]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][6]}</span>
             </span>
-            <span>
-              {grid[i - 1][7]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][7]}</span>
             </span>
-            <span>
-              {grid[i - 1][8]}
+            <span className="cell">
+              <span className="text">{grid[i - 1][8]}</span>
             </span>
           </div>
         </div>
       )
 
+      //IF we can break, break.
       if (can_break) {
         final_html.push(<div key={"break_" + i} className="break"></div>)
       }
+      //Always reset.
+      can_break = false;
     }
-
     return final_html;
   }
-
 
   return (
     <section className="App">
