c6044bb2376bbdc9a2f530b64845cdde5a4ac586
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-12 19:56:57 -0400

    setup handler for clicking on grid spaces

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ function App() {
 
   const [grid, setGrid] = useState(sudoku);
 
+
+  const clickHandler = (e: any) => {
+    console.log('TEST');
+    console.log(e.target.dataset.grid.slice(0, 1));
+    let stateCopy = [...grid];
+    stateCopy[e.target.dataset.grid.slice(0, 1)][e.target.dataset.grid.slice(-1)] = 10;
+    setGrid(stateCopy);
+
+  }
+
   const gridHelper = (index: number) => {
 
     let can_break: boolean = false;
@@ -23,35 +33,35 @@ function App() {
       <table key={"table_" + index}>
         <tbody>
           <tr>
-            <td>
+            <td data-grid={index + "0"}>
               {grid[index][0]}
             </td>
-            <td>
+            <td data-grid={index + "1"}>
               {grid[index][1]}
             </td>
-            <td>
+            <td data-grid={index + "2"}>
               {grid[index][2]}
             </td>
           </tr>
           <tr>
-            <td>
+            <td data-grid={index + "3"}>
               {grid[index][3]}
             </td>
-            <td>
+            <td data-grid={index + "4"}>
               {grid[index][4]}
             </td>
-            <td>
+            <td data-grid={index + "5"}>
               {grid[index][5]}
             </td>
           </tr>
           <tr>
-            <td>
+            <td data-grid={index + "6"}>
               {grid[index][6]}
             </td>
-            <td>
+            <td data-grid={index + "7"}>
               {grid[index][7]}
             </td>
-            <td>
+            <td data-grid={index + "8"}>
               {grid[index][8]}
             </td>
           </tr>
@@ -69,7 +79,7 @@ function App() {
 
 
   return (
-    <section className="App">
+    <section onClick={clickHandler} className="App">
       {grid.map((array, index) => gridHelper(index))}
 
     </section>
